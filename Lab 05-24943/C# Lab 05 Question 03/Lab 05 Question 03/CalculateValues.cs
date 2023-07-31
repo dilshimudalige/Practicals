541a3ca3f964f4fe3c313878c5e4045188ef72c1
dilshimudalige <140709474+dilshimudalige@users.noreply.github.com>
2023-07-31 10:40:11 +0530

    Add files via upload

---
+++ Lab 05-24943/C# Lab 05 Question 03/Lab 05 Question 03/CalculateValues.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_05_Question_03
+{
+    internal class CalculateValues
+    {
+            public int addition(int number1, int number2)
+            {
+                return number1 + number2;
+            }
+            public int subtraction(int number1, int number2)
+            {
+                return number1 - number2;
+            }
+            public int multiplication(int number1, int number2)
+            {
+                return number1 * number2;
+            }
+            public int division(int number1, int number2)
+            {
+                return number1 / number2;
+
+                Console.ReadKey();
+            }
+        }
+}
